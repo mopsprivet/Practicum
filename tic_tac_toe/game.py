ff032db7b0f08bcf3d594e9c5b1cb52e81962562
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-07-20 01:08:12 +0500

    change: game.py file - program optimized through input()

--- tic_tac_toe/game.py
+++ tic_tac_toe/game.py
@@ -1,11 +1,17 @@
-from gameparts import Board 
+# game.py
+
+from gameparts import Board
 
 def main():
     game = Board()
     game.display()
-    game.make_move(1, 1, 'X')
+    # Тут пользователь вводит координаты ячейки.
+    row = int(input('Введите номер строки: '))
+    column = int(input('Введите номер столбца: '))
+    # В метод make_move передаются те координаты, которые ввёл пользователь.
+    game.make_move(row, column, 'X')
     print('Ход сделан!')
     game.display()
 
 if __name__ == '__main__':
-    main() 
+    main()
