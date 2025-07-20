8fba09a99098c43cfca9fdd17e3fe0153137d31a
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-07-21 02:57:30 +0500

    add: method to check if the board is full in parts.py file

--- tic_tac_toe/gameparts/parts.py
+++ tic_tac_toe/gameparts/parts.py
@@ -12,4 +12,16 @@ class Board:
     def display(self):
         for row in self.board:
             print('|'.join(row))
-            print('-' * 5)
+            print('-' * 5) 
+
+    def is_board_full(self):
+        # Цикл проходится по всем столбцам игрового поля.
+        for i in range(self.field_size):
+            # А потом по всем строчкам.
+            for j in range(self.field_size):
+                # Если находит свободную ячейку...
+                if self.board[i][j] == ' ':
+                    # ...игра продолжается.
+                    return False
+        # Иначе - ничья!
+        return True
