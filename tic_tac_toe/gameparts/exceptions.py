08faf5de4b1e1e3065e3cfe86f5c90a1591ba8dc
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-07-21 02:42:11 +0500

    change: exceptions in game.py update

---
+++ tic_tac_toe/gameparts/exceptions.py
@@ -0,0 +1,9 @@
+class FieldIndexError(IndexError):
+    """Выбрасывается, если выбрано значение вне поля.""" 
+
+    def __init__(
+        self,
+        # Текст по умолчанию.
+        message='Введено значение за границами игрового поля!'  
+    ):
+        super().__init__(message)  
