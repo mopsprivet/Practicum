db6a4f90a490f405367853a1b68f7e51d5019cd7
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-07-21 02:54:13 +0500

    update: bug fix in board class

--- tic_tac_toe/gameparts/parts.py
+++ tic_tac_toe/gameparts/parts.py
@@ -1,8 +1,10 @@
 class Board: 
     """Класс, который описывает игровое поле."""
 
-    def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
+    def __init__(self): 
+        self.field_size = 3
+        self.board = [[' ' for _ in range(self.field_size)] for _ in range(self.field_size)]
+
 
     def make_move(self, row, col, player):
         self.board[row][col] = player
