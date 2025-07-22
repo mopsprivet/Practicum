f1a06de6b0902a2e4359c5302548aada389c43d4
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-07-22 20:30:41 +0500

    change: folder's name

--- tic_tac_toe/gameparts/parts.py
+++ tic_tac_toe/gameparts/parts.py
@@ -7,4 +7,37 @@ class Board:
 
 
     def make_move(self, row, col, player):
-        sel
+        self.board[row][col] = player
+
+    def display(self):
+        for row in self.board:
+            print('|'.join(row))
+            print('-' * 5) 
+
+    def is_board_full(self):
+        # Цикл проходится по всем столбцам игрового поля.
+        for i in range(self.field_size):
+            # А потом по всем строчкам.
+            for j in range(self.field_size):
+                # Если находит свободную ячейку...
+                if self.board[i][j] == ' ':
+                    # ...игра продолжается.
+                    return False
+        # Иначе - ничья!
+        return True 
+    
+
+
+    def check_win(self, player):
+        # Проверка по горизонталям и вертикалям
+        for i in range(self.field_size):
+            if (all([self.board[i][j] == player for j in range(self.field_size)]) or
+                    all([self.board[j][i] == player for j in range(self.field_size)])):
+                return True
+    
+        # Проверка по диагоналям
+        if (all([self.board[i][i] == player for i in range(self.field_size)]) or
+                all([self.board[i][self.field_size - 1 - i] == player for i in range(self.field_size)])):
+            return True
+    
+        return False 
