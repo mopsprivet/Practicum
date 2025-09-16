25e56e5f097e44407cbd85e564a9649660c10040
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-09-17 00:02:16 +0500

    add: algorithms sprint's final task: 'min_platforms'

---
+++ Algorithms_and_data_structures/min_platforms_final_task.py
@@ -0,0 +1,25 @@
+# id посылки: 142434290
+
+def min_amount_of_platforms(weights: list[int], limit: int) -> int:
+    """Функция для поиска минимального кол-ва платформ
+    для перевозки роботов."""
+    
+    sorted_weights: list[int] = sorted(weights)
+    num_of_robot_pairs: int = 0 
+    easier_robot: int = 0 
+    heavier_robot: int = len(sorted_weights) - 1 
+    
+    while easier_robot < heavier_robot:
+        if sorted_weights[easier_robot] + sorted_weights[heavier_robot] <= limit:
+            num_of_robot_pairs += 1
+            easier_robot += 1
+        heavier_robot -= 1
+    
+    num_of_platforms = len(weights) - num_of_robot_pairs 
+    return num_of_platforms
+
+
+if __name__ == '__main__': 
+    weights: list[int] = [int(x) for x in input().split()]
+    limit: int = int(input()) 
+    print(min_amount_of_platforms(weights, limit)) 
