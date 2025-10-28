cf4006be9962554e5c60cfa76689eaaa8fee1e27
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-10-29 04:58:46 +0500

    add: models to ice_cream application

--- Django/anfisa1sprint/anfisa_for_friends/ice_cream/models.py
+++ Django/anfisa1sprint/anfisa_for_friends/ice_cream/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=64, unique=True)
+    output_order = models.PositiveSmallIntegerField(default=100)
+    is_published = models.BooleanField(default=True)
+
+class Wrapper(models.Model): 
+    title = models.CharField(max_length=256) 
+    is_published = models.BooleanField(default=True)
+
+class Topping(models.Model): 
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=64, unique=True)
+    is_published = models.BooleanField(default=True)
+
+class IceCream(models.Model): 
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    is_on_main = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
