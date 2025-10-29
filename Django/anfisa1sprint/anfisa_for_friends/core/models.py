b48f19886caf0f704cb7c5582a8097a3d199cede
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-10-29 23:22:48 +0500

    add: code refactoring: наследование от абстрактного класса (перенесено одно значение в него)

--- Django/anfisa1sprint/anfisa_for_friends/core/models.py
+++ Django/anfisa1sprint/anfisa_for_friends/core/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 
-# Создайте абстрактную PublishedModel модель тут
+# Создайте абстрактную PublishedModel модель тут 
+class PublishedModel(models.Model): 
+    is_published = models.BooleanField(default=True) 
+
+    class Meta: 
+        abstract = True 
+
--- Django/anfisa1sprint/anfisa_for_friends/ice_cream/models.py
+++ Django/anfisa1sprint/anfisa_for_friends/ice_cream/models.py
@@ -1,25 +1,23 @@
 from django.db import models
 
-class Category(models.Model):
+from core.models import PublishedModel
+
+class Category(PublishedModel):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
     output_order = models.PositiveSmallIntegerField(default=100)
-    is_published = models.BooleanField(default=True)
 
-class Wrapper(models.Model): 
+class Wrapper(PublishedModel): 
     title = models.CharField(max_length=256) 
-    is_published = models.BooleanField(default=True)
 
-class Topping(models.Model): 
+class Topping(PublishedModel): 
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=64, unique=True)
-    is_published = models.BooleanField(default=True)
 
-class IceCream(models.Model): 
+class IceCream(PublishedModel): 
     title = models.CharField(max_length=256)
     description = models.TextField()
     is_on_main = models.BooleanField(default=False)
-    is_published = models.BooleanField(default=True)
-    wrapper = models.OneToOneField(Wrapper, on_delete=models.SET_NULL, null=True)
+    wrapper = models.OneToOneField(Wrapper, on_delete=models.SET_NULL, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     toppings = models.ManyToManyField(Topping)
