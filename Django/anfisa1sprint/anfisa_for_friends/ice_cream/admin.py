f162ea0d482e2499abe6ac4de6deef22f8a26fe7
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-10-30 20:58:34 +0500

    add: Topping, Wrapper and IceCream models are registrated in admin-pannel

--- Django/anfisa1sprint/anfisa_for_friends/ice_cream/admin.py
+++ Django/anfisa1sprint/anfisa_for_friends/ice_cream/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Topping, Wrapper, IceCream 
 
 admin.site.register(Category) 
+admin.site.register(Topping) 
+admin.site.register(Wrapper) 
+admin.site.register(IceCream) 
