a66208c204cca5da50003d116bb759cce116e419
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-10-31 20:57:42 +0500

    add: добавлено условие для отображения  на главной странице в filter()

--- Django/anfisa1sprint/anfisa_for_friends/homepage/views.py
+++ Django/anfisa1sprint/anfisa_for_friends/homepage/views.py
@@ -5,7 +5,7 @@ from ice_cream.models import IceCream
 def index(request):
     template = 'homepage/index.html'
     # Запрос: # Верни только те объекты, у которых в поле is_on_main указано True: 
-    ice_cream_list = IceCream.objects.values('title', 'id', 'description').filter(is_on_main=True)
+    ice_cream_list = IceCream.objects.values('title', 'id', 'description').filter(is_published=True, is_on_main=True)
     
     # Полученный из БД QuerySet передаём в словарь контекста:
     context = {
