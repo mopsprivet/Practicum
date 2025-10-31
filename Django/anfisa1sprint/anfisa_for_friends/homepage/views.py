d0bd68df2c00f7709410bb8bc33d7ed7621b09ea
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-10-31 20:31:35 +0500

    fix

--- Django/anfisa1sprint/anfisa_for_friends/homepage/views.py
+++ Django/anfisa1sprint/anfisa_for_friends/homepage/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 
+from ice_cream.models import IceCream 
 
 def index(request):
     template = 'homepage/index.html'
     # Запрос:
-    ice_cream_list = IceCream.objects.all()
+    ice_cream_list = IceCream.objects.values('title', 'id', 'description')
     # Полученный из БД QuerySet передаём в словарь контекста:
     context = {
         'ice_cream_list': ice_cream_list,
