c11a64633f573830100b2ca91b41b7229f107740
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-10-31 20:15:08 +0500

    add: получение полного списка мороженого для отображения на главной странице (запрос в view-функции главной страницы homepage)

--- Django/anfisa1sprint/anfisa_for_friends/homepage/views.py
+++ Django/anfisa1sprint/anfisa_for_friends/homepage/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+    # Запрос:
+    ice_cream_list = IceCream.objects.all()
+    # Полученный из БД QuerySet передаём в словарь контекста:
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
+    return render(request, template, context) 
