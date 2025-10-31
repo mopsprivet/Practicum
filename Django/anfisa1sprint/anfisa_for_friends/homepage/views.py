620a9dbd83099bbe8e82ad5d7eef5779dc56f08b
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-10-31 21:21:31 +0500

    add: сортировка запроса

--- Django/anfisa1sprint/anfisa_for_friends/homepage/views.py
+++ Django/anfisa1sprint/anfisa_for_friends/homepage/views.py
@@ -6,8 +6,10 @@ from ice_cream.models import IceCream
 def index(request):
     template = 'homepage/index.html'
     # Запрос: # Верни только те объекты, у которых в поле is_on_main указано True: 
-    ice_cream_list = IceCream.objects.values('title', 'id', 'description').filter(Q(is_published=True) & Q(is_on_main=True)
-)
+    ice_cream_list = IceCream.objects.values('id', 'title', 'description'
+        ).filter(
+            is_published=True, is_on_main=True
+        ).order_by('title')[1:4] 
     
     # Полученный из БД QuerySet передаём в словарь контекста:
     context = {
