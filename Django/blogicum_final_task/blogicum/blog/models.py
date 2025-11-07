454dfe7194388c35492b143b7d026646d8ee81e4
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-07 18:00:18 +0500

    add: текстовые подсказки для некоторых полей

--- Django/blogicum_final_task/blogicum/blog/models.py
+++ Django/blogicum_final_task/blogicum/blog/models.py
@@ -5,7 +5,10 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class PublishedCreatedModel(models.Model):
-    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    is_published = models.BooleanField(
+        default=True,
+        verbose_name='Опубликовано',
+        help_text='Снимите галочку, чтобы скрыть публикацию.')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Добавлено')
 
     class Meta:
@@ -15,7 +18,10 @@ class PublishedCreatedModel(models.Model):
 class Category(PublishedCreatedModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(unique=True, verbose_name='Идентификатор')
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Идентификатор',
+        help_text='Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
 
     class Meta:
         verbose_name = 'категория'
@@ -39,7 +45,9 @@ class Location(PublishedCreatedModel):
 class Post(PublishedCreatedModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateTimeField(verbose_name='Дата и время публикации')
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        help_text='Если установить дату и время в будущем — можно делать отложенные публикации.')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
