2d04d7aca9645195f2bc3a26f37d251873d87d2b
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-14 19:10:59 +0500

    add: details to views.py

--- Django/birthday/acme_project/birthday/views.py
+++ Django/birthday/acme_project/birthday/views.py
@@ -4,7 +4,19 @@ from .forms import BirthdayForm
 
 
 def birthday(request):
-    print(request.GET)
-    form = BirthdayForm()
+    # Если есть параметры GET-запроса...
+    if request.GET:
+        # ...передаём параметры запроса в конструктор класса формы.
+        form = BirthdayForm(request.GET)
+        # Если данные валидны...
+        if form.is_valid():
+            # ...то считаем, сколько дней осталось до дня рождения.
+            # Пока функции для подсчёта дней нет — поставим pass:
+            pass
+    # Если нет параметров GET-запроса.
+    else:
+        # То просто создаём пустую форму.
+        form = BirthdayForm()
+    # Передаём форму в словарь контекста:
     context = {'form': form}
     return render(request, 'birthday/birthday.html', context)
