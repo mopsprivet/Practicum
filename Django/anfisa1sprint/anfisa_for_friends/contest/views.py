73f48e8e77c6d75286f10458753f7572d1ba3796
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-14 22:54:35 +0500

    fix

--- Django/anfisa1sprint/anfisa_for_friends/contest/views.py
+++ Django/anfisa1sprint/anfisa_for_friends/contest/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from .forms import ContestForm
 
 
-def proposal_create(request):
-    form = ContestForm()
+def proposal(request):
+    if request.GET:
+        form = ContestForm(request.GET)
+        if form.is_valid():
+            pass
+    else:
+        form = ContestForm()
     return render(request, 'contest/form.html', {'form': form})
-
-
-def accepted(request):
-    return render(request, 'contest/proposal_accepted.html')
