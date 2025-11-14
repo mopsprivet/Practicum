59f4f7274237076c417333bf5e3f7ff24ded24c7
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-14 19:03:21 +0500

    add: print request

--- Django/birthday/acme_project/birthday/views.py
+++ Django/birthday/acme_project/birthday/views.py
@@ -5,9 +5,7 @@ from .forms import BirthdayForm
 
 
 def birthday(request):
-    # Создаём экземпляр класса формы.
+    print(request.GET)
     form = BirthdayForm()
-    # Добавляем его в словарь контекста под ключом form:
     context = {'form': form}
-    # Указываем нужный шаблон и передаём в него словарь контекста.
-    return render(request, 'birthday/birthday.html', context) 
+    return render(request, 'birthday/birthday.html', context)
