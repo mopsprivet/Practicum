d9b700cc6b2af4d966de23dc9333dae0ff0ffa41
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-14 23:43:52 +0500

    change: forms.py for context

--- Django/anfisa1sprint/anfisa_for_friends/contest/forms.py
+++ Django/anfisa1sprint/anfisa_for_friends/contest/forms.py
@@ -1,31 +1,21 @@
-import django.forms as forms
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
+from .models import Contest  
 
-
-class ContestForm(forms.Form):
-    title = forms.CharField(
-        label='Название',
-        max_length=20,
-        required=True,
-        widget=forms.TextInput(),
-    )
-
-    description = forms.CharField(
-        label='Описание',
-        required=True,
-        widget=forms.Textarea(attrs={'rows': 10, 'cols': 40}),
-    )
-
-    price = forms.IntegerField(
-        label='Цена',
-        required=True,
-        min_value=10,
-        max_value=100,
-        help_text='Рекомендованная розничная цена',
-        widget=forms.NumberInput(),
-    )
-
-    comment = forms.CharField(
-        label='Комментарий',
-        required=False,
-        widget=forms.Textarea(attrs={'rows': 7, 'cols': 40}),
-    )
+class ContestForm(forms.ModelForm):
+    class Meta:
+        model = Contest
+        fields = ['title', 'description', 'price', 'comment']
+        labels = {
+            'title': 'Название',
+            'description': 'Описание',
+            'price': 'Цена',
+            'comment': 'Комментарий',
+        }
+        help_texts = {
+            'price': 'Рекомендованная розничная цена',
+        }
+        widgets = {
+            'description': forms.Textarea(attrs={'cols': '22', 'rows': '5'}),
+            'comment': forms.Textarea(attrs={'cols': '22', 'rows': '5'}),
+        }
