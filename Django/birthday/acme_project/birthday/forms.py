dc2fb9110e4489144698c8f596ff556a9a1f5396
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-14 18:40:33 +0500

    add: details fir birthday form

--- Django/birthday/acme_project/birthday/forms.py
+++ Django/birthday/acme_project/birthday/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class BirthdayForm(forms.Form):
-    first_name = forms.CharField()
-    last_name = forms.CharField()
-    birthday = forms.DateField() 
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(required=False)
+    birthday = forms.DateField()
