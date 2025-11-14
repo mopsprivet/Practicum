28503ab108dfd6ff911afb216a8eb18fd967693d
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-15 00:29:20 +0500

    add: editing and deleting objects via the form

--- Django/anfisa1sprint/anfisa_for_friends/contest/urls.py
+++ Django/anfisa1sprint/anfisa_for_friends/contest/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'contest'
 
 urlpatterns = [
-    path('', views.proposal_create, name='create'),
-    path('accepted/', views.accepted, name='accepted'),
-] 
+    path('', views.proposal, name='create'),
+    path('list/', views.proposal_list, name='list'),
+    path('<int:pk>/edit/', views.proposal, name='edit'),
+    path('<int:pk>/delete/', views.delete_proposal, name='delete'),
+]
--- Django/anfisa1sprint/anfisa_for_friends/contest/views.py
+++ Django/anfisa1sprint/anfisa_for_friends/contest/views.py
@@ -1,23 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import ContestForm
 from .models import Contest
 
-def proposal(request):
-    form = ContestForm(request.POST or None)
-    saved_contest = None
+def proposal(request, pk=None):
+    if pk is not None:
+        instance = get_object_or_404(Contest, pk=pk)
+    else:
+        instance = None
+
+    form = ContestForm(request.POST or None, instance=instance)
 
     form_is_valid = form.is_valid()
 
     if request.method == 'POST' and form_is_valid:
-        saved_contest = form.save()  
+        form.save()  
+
     context = {
         'form': form,
-        'saved_contest': saved_contest,
         'form_is_valid': form_is_valid,
     }
     return render(request, 'contest/form.html', context)
 
 
+def delete_proposal(request, pk):
+    instance = get_object_or_404(Contest, pk=pk)
+    form = ContestForm(instance=instance)
+
+    if request.method == 'POST':
+        instance.delete()
+        return redirect('contest:list')
+
+    return render(request, 'contest/form.html', {'form': form})
+
+
 def proposal_list(request):
-    contests = Contest.objects.all().order_by('id')
-    return render(request, 'contest/contest_list.html', {'contests': contests})
+    contest_proposals = Contest.objects.order_by('id')
+    return render(request, 'contest/contest_list.html', {'contest_proposals': contest_proposals})
