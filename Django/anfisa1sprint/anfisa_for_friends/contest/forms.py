f4650bd9b3392b843593a3a9e33f1ee5dec54b66
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-20 00:16:33 +0500

    add: author field in contest model

--- Django/anfisa1sprint/anfisa_for_friends/contest/forms.py
+++ Django/anfisa1sprint/anfisa_for_friends/contest/forms.py
@@ -27,7 +27,7 @@ class ContestForm(forms.ModelForm):
 
     class Meta:
         model = Contest
-        fields = '__all__'
+        exclude = ('author',)
         widgets = {
             'description': forms.Textarea({'cols': '22', 'rows': '5'}),
             'comment': forms.Textarea({'cols': '22', 'rows': '5'}),
--- Django/anfisa1sprint/anfisa_for_friends/contest/models.py
+++ Django/anfisa1sprint/anfisa_for_friends/contest/models.py
@@ -1,8 +1,13 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.db import models
+from django.db import models 
+from django.contrib.auth import get_user_model 
 
+User = get_user_model()
 
 class Contest(models.Model):
+    author = models.ForeignKey(
+        User, verbose_name='Автор записи', on_delete=models.CASCADE, null=True
+    )
     title = models.CharField(verbose_name='Название', max_length=20)
     description = models.TextField(verbose_name='Описание')
     price = models.IntegerField(
