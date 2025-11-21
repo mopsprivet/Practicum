61abd1c400e99720e30b83c71ec8e31f4376bb69
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-21 16:13:33 +0500

    add: handler fof 403

--- Django/blogicum_final_task/blogicum/blogicum/urls.py
+++ Django/blogicum_final_task/blogicum/blogicum/urls.py
@@ -26,3 +26,6 @@ urlpatterns = [
 
 handler404 = 'core.views.page_not_found' 
 
+handler403 = 'core.views.csrf_failure' 
+
+
--- Django/blogicum_final_task/blogicum/core/views.py
+++ Django/blogicum_final_task/blogicum/core/views.py
@@ -4,4 +4,4 @@ def page_not_found(request, exception):
     return render(request, 'pages/404.html', status=404)
 
 def csrf_failure(request, reason=''):
-    return render(request, 'pages/403csrf.html', status=403) 
+    return render(request, 'pages/403csrf.html', status=403)
