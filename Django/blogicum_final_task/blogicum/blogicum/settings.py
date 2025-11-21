11bc0606d9ca6bcb90ffad4bd0a6015ba94fbbba
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-21 15:44:07 +0500

    add: core app

--- Django/blogicum_final_task/blogicum/blogicum/settings.py
+++ Django/blogicum_final_task/blogicum/blogicum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#c-9kkpno5l8lzhy%#f9ahd9fi^j7u+hmlr@bs1mpbs$k^3722
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 
 # Application definition
