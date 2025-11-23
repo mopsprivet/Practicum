8e4e68b31be8c8ecb5f3ee3c53ec771aafe2cc3e
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-23 21:38:55 +0500

    change: debug = false to show customized error messages

--- Django/blogicum_final_task/blogicum/blogicum/settings.py
+++ Django/blogicum_final_task/blogicum/blogicum/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#c-9kkpno5l8lzhy%#f9ahd9fi^j7u+hmlr@bs1mpbs$k^3722'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['*']
 
 CSRF_FAILURE_VIEW = 'core.views.csrf_failure' 
 
