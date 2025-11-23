3c2fb48b71b92544e7b6af06c332215c8f6dca0e
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-23 21:26:34 +0500

    fix

--- Django/blogicum_final_task/blogicum/blogicum/urls.py
+++ Django/blogicum_final_task/blogicum/blogicum/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls import include, path, reverse_lazy 
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,7 +30,7 @@ urlpatterns = [
         CreateView.as_view(
             template_name='registration/registration_form.html',
             form_class=UserCreationForm,
-            success_url=reverse_lazy('pages:homepage'),
+            success_url=reverse_lazy('blog:index'),
         ),
         name='registration',
     ),
