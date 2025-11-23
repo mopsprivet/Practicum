5052d8891999750a9c1d6c47e2f59c637e3de09b
Анастасия Ситченко <sitchenko.aa@gmail.com>
2025-11-23 21:24:20 +0500

    add: auth/ and auth/registration url paths

--- Django/blogicum_final_task/blogicum/blogicum/urls.py
+++ Django/blogicum_final_task/blogicum/blogicum/urls.py
@@ -21,7 +21,16 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('pages/', include('pages.urls')),
-
+    path('auth/', include('django.contrib.auth.urls')), 
+    path(
+        'auth/registration/', 
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('pages:homepage'),
+        ),
+        name='registration',
+    ),
 ]
 
 handler404 = 'core.views.page_not_found'
